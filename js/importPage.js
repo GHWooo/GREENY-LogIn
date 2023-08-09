7da22bf2db82b229417be8f68d053f1627a0c491
GHWooo <ghw9174@gmail.com>
2023-08-09 18:10:29 +0900

    feat: 경로에 따른 요소를 불러오기 위해 메서드 수정

--- js/importPage.js
+++ js/importPage.js
@@ -2,6 +2,6 @@ async function fetchHtmlAsText(url) {
     return await (await fetch(url)).text();
 }
 
-async function importPage(target) {
-    document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(target + '.html');
+async function importPage(target, path) {
+    document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(path?`${path}${target}.html`:`${target}.html`);
 }
