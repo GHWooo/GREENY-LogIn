0e9e465198b2b66447967464949dc344a83607e0
GHWooo <ghw9174@gmail.com>
2023-10-29 21:32:32 +0900

    update(importPage.js): 컴포넌트를 불러오는 로직 수정 및 분리

--- js/importPage.js
+++ js/importPage.js
@@ -1,7 +1,33 @@
 async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
+  return await (await fetch(url)).text();
 }
 
 async function importPage(target, path, distinguished) {
-    document.querySelector(`#${distinguished||target}`).innerHTML = await fetchHtmlAsText(path?`${path?path:''}${target}.html`:`${target}.html`);
+  document.querySelector(`#${distinguished || target}`).innerHTML = await fetchHtmlAsText(
+    path ? `${path ? path : ''}${target}.html` : `${target}.html`
+  );
+}
+
+function importComponentCategories() {
+  const componentCategories = {
+    pupil: ['left_pupil', 'right_pupil'],
+    eye: ['left_eye', 'right_eye'],
+    closeEye: ['left_close_eye', 'right_close_eye'],
+    wing: ['left_wing', 'right_wing'],
+    shoes: ['left_shoes', 'right_shoes'],
+    topButton,
+    band,
+    hat,
+    leftLeg,
+    rightLeg,
+  };
+
+  Object.keys(componentCategories).forEach((category_key) => {
+    if (!componentCategories[category_key].length) importPage(category_key, 'components/');
+    else {
+      componentCategories[category_key].forEach((componentType) => {
+        importPage(category_key, 'components/', componentType);
+      });
+    }
+  });
 }
